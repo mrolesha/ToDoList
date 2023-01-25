e95372a126f68840f36b93577cd997024d852789
mrolesha <116891703+mrolesha@users.noreply.github.com>
2023-01-25 02:25:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,23 @@ const buttonAddElement = document.querySelector('.js-buttonadd');
 
 const list = document.querySelector('.js-ToDoListRow');
 
-const forms = document.querySelector('.form__row')
+const forms = document.querySelector('.form__row');
+
+const circle = document.querySelector('.circle');
+
+const arrow = forms.querySelector('.arrow-js');
+
+const switchFooter = document.querySelector('.footer-js');
+
+const footerCount = forms.querySelector('.footer__count');
+
+const reset = forms.querySelector('.js-clear');
+
+const all = forms.querySelector('.js-all');
+
+const active = forms.querySelector('.js-active');
+
+const completed = forms.querySelector('.js-completed');
 
 const form = document.forms.main;
 
@@ -12,7 +28,11 @@ const inputName = form.input;
 
 const textOfField = [];
 
+const elements = [];
+
 let i = 0;
+
+let arrowCount = 0;
 
 //All function
 
@@ -56,23 +76,21 @@ function resetElements(){
        
 }
 
-function elementAdd(){
+function elementAdd(string){
     
-    const string = textOfField.pop();
-    
-    if(!string){
-        
-        alert('Field is empty');
-        
-     }else{
-         
         const elementOfList = document.createElement('div');
          
         elementOfList.classList.add('element__row');
     
         list.append(elementOfList);
-         
+    
+        const circle = document.createElement('div');
+    
+        circle.innerHTML = `<img class="circle" src="img/circle.png" alt="">`;
+        
         elementOfList.innerHTML = `<div class="element__text"> ${string}</div>`;
+    
+        elementOfList.prepend(circle);
 
         const crest = document.createElement("div");
          
@@ -81,11 +99,31 @@ function elementAdd(){
         crest.innerHTML = `<img class="img js-elementbutton" src="img/crest.png" alt="">`;
          
         elementOfList.append(crest);
-         
-        resetElements();
-         
+    
+        arrow.classList.add('arrow');
+    
+        checkCount();
+        
+        switchFooter.classList.add('formon');
+    
+        elements.push(elementOfList);
+    
         clearTextOfField();
          
+}
+
+function checkCount(){
+    
+    arrowCount++;
+    
+    if(arrowCount == 1){
+        
+        footerCount.innerHTML = `${arrowCount} item left`;
+        
+    }else{
+        
+        footerCount.innerHTML = `${arrowCount} items left`;
+        
     }
     
 }
@@ -100,21 +138,49 @@ function remove(target){
 
     target.remove();
     
+    --arrowCount;
+    
+    if(arrowCount == 0){
+        
+        arrow.classList.remove('arrow');
+       
+        switchFooter.classList.remove('formon');
+        
+        footerCount.innerHTML = `${arrowCount} item left`;
+        
+    }else{
+        
+        footerCount.innerHTML = `${arrowCount} items left`;
+        
+    }
+    
 }
 
 //All events
 
-inputName.addEventListener("input", function(e){
+inputName.addEventListener('keydown', function(e){
     
-   changeTextOfField(inputName.value);
+    if(e.code != 'Enter'){
+        
+        inputName.addEventListener("input", function(e){
     
-});
-
-buttonAddElement.addEventListener('click', function(e){
+            changeTextOfField(inputName.value);
     
-    e.preventDefault();  
+        });
+  
+    }else{
+        
+        e.preventDefault();  
+        
+        const string = textOfField.pop();
+        
+        if(string.length>=1){
+            
+            elementAdd(string);
     
-    elementAdd();
+        }
+        
+    }
     
 });
 
@@ -126,17 +192,30 @@ list.addEventListener('click', function({target}){
     
     if(target.classList.contains('element__row')){
         
-        setStyle(target)
+        setStyle(target);
         
     }
     
     if(target.classList.contains('js-elementbutton')){
         
-        remove(target.closest('.element__row'))
-    
+        remove(target.closest('.element__row'));
+        
     }
-    
     
 });
 
-
+reset.addEventListener('click', function(e){
+    
+    e.preventDefault();
+    
+    list.innerHTML = `` ;
+    
+    arrow.classList.remove('arrow');
+       
+    switchFooter.classList.remove('formon');
+    
+    arrowCount = 0 ;
+    
+    i = 0 ;
+    
+});
